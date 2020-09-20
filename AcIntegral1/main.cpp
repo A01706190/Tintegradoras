473d1770866fc92998654d619a28de74001617f1
A01706190 <65308518+A01706190@users.noreply.github.com>
2020-09-20 17:20:49 -0500

    Update main.cpp

--- AcIntegral1/main.cpp
+++ AcIntegral1/main.cpp
@@ -21,7 +21,48 @@ void menu(int& opcion)
     <<"3.- Buscar rango\n"<<"4.- Imprimir en pantalla\n"<<"5.- Guardar en txt\n"<<"6.- Salir\n";
     cin>>opcion;
 }
-
+void prueba()
+{
+    vector<string> v;
+    File file("mapa.txt");
+    v = file.read();
+    
+//    Caso 1
+    cout<<endl<<"CASO 1"<< endl;
+    int arr1[2] = {1,1};
+    Map mapa1(v,arr1);
+    mapa1.sort();
+    cout<<mapa1.toString();
+//    Caso 2
+    cout<<endl<<"CASO 2"<< endl;
+    int arr2[2] = {-10,-50};
+    Map mapa2(v,arr2);
+    mapa2.sort();
+    cout<<mapa2.toString();
+//    Caso 3
+    cout<<endl<<"CASO 3"<< endl;
+    int arr3[2] = {1,1};
+    Map mapa3(v,arr3);
+    mapa3.sort();
+    cout<<mapa3.toString();
+//    Caso 4
+    cout<<endl<<"CASO 4"<< endl;
+    int arr4[2] = {1,1};
+    Map mapa4(v,arr4);
+    Map res4_1;
+    Map res4_2;
+    Map res4_3;
+    Map res4_4;
+    mapa4.sort();
+    mapa4.find(0,0,res4_1);
+    mapa4.find(0,10,res4_2);
+    mapa4.find(10,100,res4_3);
+    mapa4.find(-10,-10,res4_4);
+    cout <<"1. "<< res4_1.toString();
+    cout <<"2. "<< res4_2.toString();
+    cout <<"3. "<< res4_3.toString();
+    cout <<"4. "<< res4_4.toString();
+}
 int main(int argc, const char * argv[])
 {
     int x,y,from,to;
@@ -33,15 +74,14 @@ int main(int argc, const char * argv[])
     cin >> x;
     cout<<"Ubicacion en Y: ";
     cin >> y;
-    
+
     int point[2] = {x,y}; //punto inicial(x,y)
     vector<string> v;
     Map busca;
     File file("mapa.txt");
     v = file.read();
     Map mapita(v,point);
-//    cout <<mapita.toString();
-    
+
     while(opcion!=6)
     {
         Map busca;
@@ -50,7 +90,7 @@ int main(int argc, const char * argv[])
             case 1:
                 cout<<"Listo\n";
                 break;
-                
+
             case 2:
                 mapita.sort();
                 sorted = true;
@@ -68,7 +108,7 @@ int main(int argc, const char * argv[])
                 mapita.find(from, to, busca);
                 cout<<"\nUbicaciones encontradas:\n"<<busca.toString()<<"\n\n";
                 break;
-                
+
             case 4:
                 cout <<mapita.toString();
                 break;
@@ -80,14 +120,15 @@ int main(int argc, const char * argv[])
                 file.write(mapita.toString(), fileName);
                 cout<<"Listo\n";
                 break;
-                
+
             default:
                 opcion = 6;
                 break;
         }
-        
+
     }
-   
+    
+//   prueba();
    
     
     return 0;
